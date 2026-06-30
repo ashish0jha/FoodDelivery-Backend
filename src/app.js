730215a5d576a29a29167784dce6a27b545e778d
ashish0jha <ashish2893jnv@gmail.com>
2026-06-30 18:37:27 +0530

    saved frontend url into the .env

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser())
 app.use(cors({
-    origin: "http://localhost:1234",
+    origin: process.env.FRONT_END_URL,
     credentials: true
 }));
 
